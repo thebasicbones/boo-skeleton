9a9e010967fb8052b1705da782401d35e6cce623
nayana-vinod <nayanavinodk33@gmail.com>
2025-12-05 20:19:19 +0530

    Revert "Initial files for boo-package-manager"
    This reverts commit 97870864529397fe16cfaed28e19bc5cf1a41718.

--- boo-package-manager/app/models/__init__.py
+++
@@ -1,5 +0,0 @@
-"""Database Models"""
-
-from app.models.sqlalchemy_resource import Base, Resource, resource_dependencies
-
-__all__ = ["Resource", "resource_dependencies", "Base"]
--- boo-package-manager/app/routers/__init__.py
+++
@@ -1,5 +0,0 @@
-"""API Routers"""
-
-from app.routers import resources
-
-__all__ = ["resources"]
